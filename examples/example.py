725fca02fac3cc5c5ffe2a8159b71a85820cefc1
s-y-u-n <114199496+s-y-u-n@users.noreply.github.com>
2024-12-26 16:28:13 +0900

    実行ファイルの作成

--- examples/example.py
+++ examples/example.py
@@ -1,11 +1,11 @@
 import shap
 import xgboost
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_boston
+from sklearn.datasets import fetch_california_housing
 
 # データセットの読み込み
-boston = load_boston()
-X, y = boston.data, boston.target
+california = fetch_california_housing()
+X, y = california.data, california.target
 
 # データセットの分割
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -19,4 +19,4 @@ explainer = shap.Explainer(model)
 shap_values = explainer(X_test)
 
 # SHAP値のプロット
-shap.summary_plot(shap_values, X_test, feature_names=boston.feature_names)
+shap.summary_plot(shap_values, X_test, feature_names=california.feature_names)
