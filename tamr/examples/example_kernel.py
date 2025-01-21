6838a0dc1bd2823060a572defedbbf8dcc8c5421
s-y-u-n <114199496+s-y-u-n@users.noreply.github.com>
2025-01-21 21:30:16 +0900

    データセットのサンプル数を20に制限

--- tamr/examples/example_kernel.py
+++ tamr/examples/example_kernel.py
@@ -5,9 +5,9 @@ import xgboost
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import fetch_california_housing
 
-# データセットの読み込み
+# データセットの読み込み（20サンプルのみ）
 california = fetch_california_housing()
-X, y = california.data, california.target
+X, y = california.data[:20], california.target[:20]
 
 # データセットの分割
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
